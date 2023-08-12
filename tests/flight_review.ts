b4ebd204ec63fb7f122dfb190e5ba2bbf5639f55
Gledson <gledsoul@gmail.com>
2023-08-12 18:59:38 +0100

    addedlocal test for flight_review

--- tests/flight_review.ts
+++ tests/flight_review.ts
@@ -1,121 +1,68 @@
 import * as anchor from '@project-serum/anchor'
 import { Program, Wallet } from '@project-serum/anchor'
-import { PassengerAssets } from '../target/types/passenger_assets'
-import { TOKEN_PROGRAM_ID, createAssociatedTokenAccountInstruction, getAssociatedTokenAddress, createInitializeMintInstruction, MINT_SIZE } from '@solana/spl-token'
-const { SystemProgram } = anchor.web3
+import { FlightReview } from '../target/types/flight_review'
 
-describe('nft', () => {
-  // Configure the client to use the local cluster.
+
+
+describe('flight_review', () => {
   const provider = anchor.AnchorProvider.env();
   const wallet = provider.wallet as Wallet;
   anchor.setProvider(provider);
-  const program = anchor.workspace.PassengerAssets as Program<PassengerAssets>
+  const program = anchor.workspace.FlightReview as Program<FlightReview>
+  
+  const FLIGHT = "RBJ157";
+  
+  const REVIEW = "All was perfect!";
+  
+  const RATING = 5;
+  
 
   it("Is initialized!", async () => {
     // Add your test here.
-
-    const TOKEN_METADATA_PROGRAM_ID = new anchor.web3.PublicKey(
-      "6hogsjh86ChX1v6WPyZ6R3Vh8HLsAfVX8SKdu9j1DX3N"
-    );
-    const lamports: number =
-      await program.provider.connection.getMinimumBalanceForRentExemption(
-        MINT_SIZE
-      );
-    const getMetadata = async (
-      mint: anchor.web3.PublicKey
-    ): Promise<anchor.web3.PublicKey> => {
-      return (
-        await anchor.web3.PublicKey.findProgramAddress(
-          [
-            Buffer.from("metadata"),
-            TOKEN_METADATA_PROGRAM_ID.toBuffer(),
-            mint.toBuffer(),
-          ],
-          TOKEN_METADATA_PROGRAM_ID
-        )
-    i  )[0];
-    };
-
-    const getMasterEdition = async (
-      mint: anchor.web3.PublicKey
-    ): Promise<anchor.web3.PublicKey> => {
-      return (
-        await anchor.web3.PublicKey.findProgramAddress(
-          [
-            Buffer.from("metadata"),
-            TOKEN_METADATA_PROGRAM_ID.toBuffer(),
-            mint.toBuffer(),
-            Buffer.from("edition"),
-          ],
-          TOKEN_METADATA_PROGRAM_ID
-        )
-      )[0];
-    };
-
-    const mintKey: anchor.web3.Keypair = anchor.web3.Keypair.generate();
-    const NftTokenAccount = await getAssociatedTokenAddress(
-      mintKey.publicKey,
-      wallet.publicKey
-    );
-    console.log("NFT Account: ", NftTokenAccount.toBase58());
-
-    const mint_tx = new anchor.web3.Transaction().add(
-      anchor.web3.SystemProgram.createAccount({
-        fromPubkey: wallet.publicKey,
-        newAccountPubkey: mintKey.publicKey,
-        space: MINT_SIZE,
-        programId: TOKEN_PROGRAM_ID,
-        lamports,
-      }),
-      createInitializeMintInstruction(
-        mintKey.publicKey,
-        0,
-        wallet.publicKey,
-        wallet.publicKey
-      ),
-      createAssociatedTokenAccountInstruction(
-        wallet.publicKey,
-        NftTokenAccount,
-        wallet.publicKey,
-        mintKey.publicKey
-      )
-    );
-
-    const res = await program.provider.sendAndConfirm(mint_tx, [mintKey]);
-    console.log(
-      await program.provider.connection.getParsedAccountInfo(mintKey.publicKey)
-    );
-
-    console.log("Transaction: ", res);
-    console.log("Associated Token Account -ATA - Will attach metadata to the Token: ", mintKey.publicKey.toString());
-    console.log("Wallet - The Flight -Payer", wallet.publicKey.toString());
+    //console.log(program);
     
-    
-    const metadataAddress = await getMetadata(mintKey.publicKey);
-    const masterEdition = await getMasterEdition(mintKey.publicKey);
+   const [REVIEW_PDA] = await anchor.web3.PublicKey.findProgramAddress(
+  [Buffer.from(FLIGHT), wallet.publicKey.toBuffer()],
+   program.programId
+);
+ 
+ console.log(`Reviewer: ${wallet.publicKey.toString()}`);
+ console.log(`Review PDA: ${REVIEW_PDA.toString()}`);
+ 
+ 
+ /*The instruction bellow will only work on DevNet*/
+ 
+// let latestBlockhash = await program.connection.getLatestBlockhash('finalized')
 
-    console.log("Metadata address: ", metadataAddress.toBase58());
-    console.log("MasterEdition: ", masterEdition.toBase58());
+ const tx = await program.methods
+  .postReview(
+    FLIGHT,
+    REVIEW,
+    RATING
+  )
+  .accounts({ review: REVIEW_PDA })
+  .rpc({
+  skipPreflight:true
+});
+  
+  /*The instruction bellow will only work on DevNet*/
+  
+ /* await pg.connection.confirmTransaction({
+  signature: tx,
+  blockhash: latestBlockhash.blockhash,
+  lastValidBlockHeight: latestBlockhash.lastValidBlockHeight
+});
+*/
 
-    const tx = await program.methods.mintNft(
-      mintKey.publicKey,
-      "https://arweave.net/y5e5DJsiwH0s_ayfMwYk-SnrZtVZzHLQDSTZ5dNRUHA",
-      "NFT Title",
-    )
-      .accounts({
-        mintAuthority: wallet.publicKey,
-        mint: mintKey.publicKey,
-        tokenAccount: NftTokenAccount,
-        tokenProgram: TOKEN_PROGRAM_ID,
-        metadata: metadataAddress,
-        tokenMetadataProgram: TOKEN_METADATA_PROGRAM_ID,
-        payer: wallet.publicKey,
-        systemProgram: SystemProgram.programId,
-        rent: anchor.web3.SYSVAR_RENT_PUBKEY,
-        masterEdition: masterEdition,
-      },
-      )
-      .rpc();
-    console.log("Your transaction signature", tx);
+/*console.log(`https://explorer.solana.com/tx/${tx}?cluster=devnet`);
+
+const data = await program.account.passenger.fetch(REVIEW_PDA);
+console.log(`Reviewer: `,data.reviewer.toString());
+console.log(`Flight: `,data.flight);
+console.log(`Review: `,data.review);
+console.log(`Rating: `,data.rating);
+  
+*/
+   
   });
 });
